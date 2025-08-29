d6acd5d3e3d3264614b54c039fd063254a5bd116
Misael Marcelino <misael.marcelino@outlook.com>
2025-08-29 18:47:36 -0300

    docs: Starting the Ekklesia project

---
+++ src/main/java/br/com/mimatech/Ekklesia/EkklesiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mimatech.Ekklesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkklesiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkklesiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mimatech/Ekklesia/EkklesiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mimatech.Ekklesia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkklesiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
